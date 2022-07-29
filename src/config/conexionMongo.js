16dfa5fa4da64391671e819be407ac22db3eaf08
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-07-29 15:09:32 -0300

    Create conexionMongo.js

---
+++ src/config/conexionMongo.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const conexionDB = async () => {
+    try {
+    
+        const db = await mongoose.connect('mongodb+srv://luis:coderhouse@cluster0.9xnml.mongodb.net/ecommerce?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Se conecto a la BD:", db.connection.name)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = conexionDB

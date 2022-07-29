a018fa0dc5e5a9e6f0eb5974a960f3e68a6fc339
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-07-29 14:57:26 -0300

    Update productsFactory.js

--- src/models/Daos/productsFactory.js
+++ src/models/Daos/productsFactory.js
@@ -3,8 +3,7 @@ const ProductsDBMongo = require('./productsDBMongo')
 class ProductsFactoryDAO {
     static get(tipo) {
         switch (tipo) {
-            case 'mongo': return new  ProductsDBMongo(/*'ecommerce','productos'*/)
-            // case 'mongo': return new  ProductsDBMongo()
+            case 'mongo': return new  ProductsDBMongo()
             default: return console.log("error")
         }
     }

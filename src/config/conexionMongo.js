295636f7ef52762de62d5a836b389df47f0d9053
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-07-29 17:49:40 -0300

    Update conexionMongo.js

--- src/config/conexionMongo.js
+++ src/config/conexionMongo.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const config = require('../config/config')
 
 const conexionDB = async () => {
     try {
-    
-        const db = await mongoose.connect('mongodb+srv://luis:coderhouse@cluster0.9xnml.mongodb.net/ecommerce?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+        const db = await mongoose.connect(`mongodb+srv://${config.NAME}:${config.PASSWORD}@cluster0.9xnml.mongodb.net/${config.NAME_DATABASE}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log("Se conecto a la BD:", db.connection.name)
+        
     } catch (error) {
         console.log(error)
     }

c4d61a8551e9cd0cf5e8db9751844ea7dd78957c
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-07-29 14:58:48 -0300

    Update products.js

--- src/models/Daos/products.js
+++ src/models/Daos/products.js
@@ -1,12 +1,66 @@
+const mongoose = require('mongoose');
+const conexionDB = require('../../config/conexionMongo')
+
+conexionDB()
+
 class ProductsBaseDAO {
-    getNext_Id(products) {
-        let longitud = products.length
-        return longitud? parseInt(products[longitud-1]._id) + 1 : 1
+    
+    constructor(collection, schema) {
+        this.collection = mongoose.model(collection, schema);
     }
 
-    getIndex(_id,products) {
-        return products.finIndex(product => product? product._id == _id: true)
+
+    async obtenerProductos(id) {
+        try {
+            if(id) {
+                const product = await this.collection.findOne({_id: id})
+                return product
+            }
+            else {
+                const products = await this.collection.find()
+                return products
+            }
+        } catch (error) {
+            console.log(error)
+        }
     }
+
+
+    async guardarProducto(product) {
+        try {
+
+            await this.collection.create(product)
+            return product
+
+        } catch (error) {
+            console.log(error)
+            return product
+        }
+    }
+
+    async actualizarProducto(id,product) {
+        try {
+
+            await this.collection.updateOne({ _id: id }, product)
+            return product
+
+        } catch (error) {
+            console.log(error)
+            return product
+        }
+    }
+
+    async eliminarProducto(id) {
+        try {
+            const deleteProduct = await this.collection.deleteOne({ _id: id })
+            return deleteProduct
+
+        } catch (error) {
+            console.log(error)
+            return deleteProduct
+        }
+    }
+
 }
 
 module.exports = ProductsBaseDAO
